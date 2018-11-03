c9d2f5f5035353bbf0f6612fce265afd7319755e
ryp008 <44718529+ryp008@users.noreply.github.com>
2018-11-03 11:25:48 +0700

    Add files via upload

---
+++ config/DB.php
@@ -0,0 +1,13 @@
+<?php
+
+class DB {
+    public static $engine="mysql";
+    public static $host="localhost";
+    public static $user="root";
+    public static $pass="root";
+    public static $dbname="pdp";
+    
+    // public static $db=array(
+    //   "mysql:host=localhost;dbname=pdp","root","root"
+    // );
+}
---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+//konfigurasi path untuk website
+define("PATH","http://localhost/pdp/");
+define("SITE_URL",PATH."web/index.php");
+define("HOME",PATH."?p=index");
